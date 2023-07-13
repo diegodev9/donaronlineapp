607d5871dc9ed57a7f020a7c8c797810bea4c1a2
diegodev9 <diegodev9@gmail.com>
2023-07-13 16:35:22 -0300

    create models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   resources :donations
-  devise_for :users
   resources :donors
   resources :companies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
